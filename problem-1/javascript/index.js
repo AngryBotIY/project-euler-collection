57ba085a0c6cc589e62bc481163dbc3a088c7768
YI <yurkimus@gmail.com>
2021-01-22 01:57:46 +0300

    remove log from problem-1 js

--- problem-1/javascript/index.js
+++ problem-1/javascript/index.js
@@ -1,7 +1,5 @@
 const INPUT = (count) => new Array(count).fill().map((x, i) => i++)
 
-const main = (data) => data.reduce((acc, value) => (value % 3 === 0 || value % 5 === 0) ? acc + value : acc, 0)
+const main = (data) => data.reduce((acc, value) => (value % 3 === 0 || value % 5 === 0 ? acc + value : acc), 0)
 
 const OUTPUT = main(INPUT(1000))
-
-console.log('OUTPUT', OUTPUT)
